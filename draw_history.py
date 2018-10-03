791083d668074654bb24e86ad12e4ee19a5d7351
Eric Noulard <enoulard@antidot.net>
2018-10-03 08:06:26 +0200

    Save figure to pdf

--- draw_history.py
+++ draw_history.py
@@ -9,10 +9,19 @@ parser.add_argument(
     'input',
     type=Path,
     help='File from which to read the text')
+parser.add_argument(
+    '--no_save',
+    action='store_true',
+    help='Save the figure to pdf file (same name) (default: %(default)s)')
 
 args = parser.parse_args()
 
 df = pandas.read_csv(args.input.open('r'))
 df = df.set_index('epoch')
 fig = df.plot()
+plt.grid(b=True, which='both')
+if not args.no_save:
+    saved_fig = args.input.with_suffix('.pdf')
+    plt.savefig(saved_fig, format='pdf')
+    print("Saved figure to: {}".format(saved_fig))
 plt.show()
